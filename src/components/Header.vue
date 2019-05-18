1fbc3aff19c157a32561bd68998f46488b86d0b9
Patrick <cgq3210@gmial.com>
2019-05-18 14:55:11 +0800

    add header zone

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,15 @@
 <template>
     <div class="header">
-        <div class="header-left">返回</div>
-        <div class="header-input">内容</div>
-        <div class="header-right">城市</div>
+        <div class="header-left">
+            <div class="iconfont back-icon">&#xe624;</div>
+        </div>
+        <div class="header-input">
+            <span class="iconfont">&#xe632;</span>
+            输入城市/景点/游玩主题</div>
+        <div class="header-right">
+            城市
+            <span class="iconfont arrow-icon">&#xe64a;</span>
+        </div>
     </div>
 </template>
 
@@ -13,20 +20,34 @@
 </script>
 
 <style lang="stylus" scoped>
+    @import '~@/assets/styles/variables.styl'
     .header
         display: flex
         line-height: .86rem //行高
-        background :#00bcd4
+        background :$bgColor
         color: #fff   //字体颜色：白色
         .header-left
             width: .64rem
             float: left
+            .back-icon
+                text-align: center
+                font-size: .4rem
         .header-input
             flex: 1
+            line-height: .64rem
+            height : .64rem
+            text-align: left
+            margin-top: .12rem
+            margin-left: .2rem
+            padding-left: .8rem
             background :#fff
-            
+            border-radius: .1rem //圆角
+            color: #ccc
         .header-right
             width: 1.24rem
             float: right
             text-align: center
+            font-size: .3rem
+            .arrow-icon
+                font-size: .26rem
 </style>
