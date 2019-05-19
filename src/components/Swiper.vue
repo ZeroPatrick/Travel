834b75b395266960c6d9e18aef66fb332af6e8f2
Patrick <cgq3210@gmial.com>
2019-05-19 08:30:29 +0800

    add swiper

---
+++ src/components/Swiper.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <!-- slides -->
+            <swiper-slide>
+                <img class="swiper-img" src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/522b43cda83d29460da14ec8975b4060.jpg_750x200_2ff9e60a.jpg" />
+            </swiper-slide>
+            <swiper-slide>
+                <img class="swiper-img" src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20195/5eb82a8ca5b0cebb2c3ae1a53a4d8f81.jpg_750x200_645bfba1.jpg" />
+            </swiper-slide>
+            <!-- Optional controls -->
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeSwiper",
+        data() {
+            return {
+                swiperOption: {}
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+    .wrapper
+        overflow :hidden
+        width: 10%
+        height:0
+        padding-bottom:32.25%
+        .swiper-img
+            width: 100%
+</style>
