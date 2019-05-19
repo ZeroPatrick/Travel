39a056c0891414a48c293cbf2cf0f1152282cb07
Patrick <cgq3210@gmial.com>
2019-05-19 16:46:15 +0800

    finish swiper

--- src/components/Swiper.vue
+++ src/components/Swiper.vue
@@ -2,11 +2,8 @@
     <div class="wrapper">
         <swiper :options="swiperOption">
             <!-- slides -->
-            <swiper-slide>
-                <img class="swiper-img" src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/522b43cda83d29460da14ec8975b4060.jpg_750x200_2ff9e60a.jpg" />
-            </swiper-slide>
-            <swiper-slide>
-                <img class="swiper-img" src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20195/5eb82a8ca5b0cebb2c3ae1a53a4d8f81.jpg_750x200_645bfba1.jpg" />
+            <swiper-slide v-for="item of swiperList" :key="item.id">
+                <img class="swiper-img" :src="item.imgUrl"/>
             </swiper-slide>
             <!-- Optional controls -->
             <div class="swiper-pagination"  slot="pagination"></div>
@@ -19,18 +16,33 @@
         name: "HomeSwiper",
         data() {
             return {
-                swiperOption: {}
+                swiperOption: {
+                    pagination: '.swiper-pagination',
+                    loop: true,
+                },
+                swiperList: [{
+                    id: "0001",
+                    imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/' +
+                        'admin/20194/522b43cda83d29460da14ec8975b4060.jpg_750x200_2ff9e60a.jpg',
+                },{
+                    id: '0002',
+                    imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/' +
+                        'admin/20195/5eb82a8ca5b0cebb2c3ae1a53a4d8f81.jpg_750x200_645bfba1.jpg',
+                }]
             }
         }
     }
 </script>
 
 <style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active
+        background :#fff
     .wrapper
         overflow :hidden
-        width: 10%
+        width: 100%
         height:0
-        padding-bottom:32.25%
+        padding-bottom:26.67%
+        background：#eee
         .swiper-img
             width: 100%
 </style>
