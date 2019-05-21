a500d799a0309c8217fa02ee8db868c1474ffb9c
name <1617534886@qq.com>
2019-05-21 13:44:54 +0800

    add recommend

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,16 +3,19 @@
       <home-header></home-header>
       <home-swiper></home-swiper>
       <home-icons></home-icons>
+      <home-recommend></home-recommend>
   </div>
 </template>
 
 <script>
-    import HomeHeader from '@/components/Header'
-    import HomeSwiper from '@/components/Swiper'
-    import HomeIcons from '@/components/Icons'
+    import HomeHeader from '../components/Header';
+    import HomeSwiper from '../components/Swiper';
+    import HomeIcons from '../components/Icons';
+    import HomeRecommend from "../components/Recommend";
     export default {
   name: 'Home',
   components: {
+      HomeRecommend,
       HomeHeader,
       HomeSwiper,
       HomeIcons,
