38015bf1abdd1a7754013668375e7e263cf3a500
name <1617534886@qq.com>
2019-05-21 14:07:43 +0800

    add recommed

--- src/components/Recommend.vue
+++ src/components/Recommend.vue
@@ -2,11 +2,14 @@
     <div>
         <div class="title">热销推荐</div>
         <ul>
-            <li class="item border-bottom">
-                <img class="item-img" src="http://img1.qunarzz.com/sight/p0/1902/84/84696f368bbec10da3.img.jpg_200x200_50323152.jpg" />
+            <li class="item border-bottom"
+                v-for="item of recommendList"
+                :key="item.id"
+            >
+                <img class="item-img" :src="item.imgUrl" />
                 <div class="item-info">
-                    <p class="item-title">北京世界园艺博览会博览会博览会博览会博览会</p>
-                    <p class="item-desc">619条评论</p>
+                    <p class="item-title">{{item.title}}</p>
+                    <p class="item-desc">{{item.desc}}</p>
                     <button class="item-button">查看详情</button>
                 </div>
             </li>
@@ -18,7 +21,30 @@
     export default {
         name: "HomeRecommend",
         data () {
-            recommendList: [{}]
+            return {
+                recommendList: [{
+                    id: '0001',
+                    imgUrl: 'http://img1.qunarzz.com/sight/p0/1902/84/' +
+                        '84696f368bbec10da3.img.jpg_200x200_50323152.jpg',
+                    title: '北京世界园艺博览会',
+                    desc: '619条评论',
+                },{
+                    id: '0002',
+                    imgUrl: 'http://img1.qunarzz.com/sight/p0/1902/84/84696f368bbec10da3.img.jpg_200x200_50323152.jpg',
+                    title: '北京世界园艺博览会',
+                    desc: '619条评论',
+                },{
+                    id: '0003',
+                    imgUrl: 'http://img1.qunarzz.com/sight/p0/1902/84/84696f368bbec10da3.img.jpg_200x200_50323152.jpg',
+                    title: '北京世界园艺博览会',
+                    desc: '619条评论',
+                },{
+                    id: '0004',
+                    imgUrl: 'http://img1.qunarzz.com/sight/p0/1902/84/84696f368bbec10da3.img.jpg_200x200_50323152.jpg',
+                    title: '北京世界园艺博览会',
+                    desc: '619条评论',
+                }]
+            }
         }
     }
 </script>
