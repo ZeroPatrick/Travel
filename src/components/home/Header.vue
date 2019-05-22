32eac45a761a91107fbf519bcae0e2e57a8825b1
name <1617534886@qq.com>
2019-05-22 16:28:00 +0800

    add city router

--- src/components/home/Header.vue
+++ src/components/home/Header.vue
@@ -6,10 +6,12 @@
         <div class="header-input">
             <span class="iconfont">&#xe632;</span>
             输入城市/景点/游玩主题</div>
-        <div class="header-right">
-            {{this.city}}
-            <span class="iconfont arrow-icon">&#xe64a;</span>
-        </div>
+        <router-link to="/city">
+            <div class="header-right">
+                {{this.city}}
+                <span class="iconfont arrow-icon">&#xe64a;</span>
+            </div>
+        </router-link>
     </div>
 </template>
 
@@ -51,6 +53,7 @@
             float: right
             text-align: center
             font-size: .3rem
+            color #fff
             .arrow-icon
                 font-size: .26rem
 </style>
